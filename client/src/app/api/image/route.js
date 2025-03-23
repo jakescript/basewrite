0ab1a11c31b58a1fbe3666d2326ee91d07db486d
jake <by.jakeg@gmail.com>
2025-03-23 12:39:49 +0100

    * optimize image gen

--- client/src/app/api/image/route.js
+++ client/src/app/api/image/route.js
@@ -4,9 +4,60 @@ import fs from "fs";
 import path from "path";
 import { GifCodec, GifFrame, GifUtil, BitmapImage } from "gifwrap";
 
-// Register your custom font from the public directory
+const endChar = '*'
+
+const gifPath = path.join(process.cwd(), "public", "disk_black_medium.gif");
+const gifBuffer = fs.readFileSync(gifPath);
+const codec = new GifCodec();
+const decodedGifPromise = codec.decodeGif(gifBuffer);
+
 const fontPath = path.join(process.cwd(), "public", "c64.ttf");
 registerFont(fontPath, { family: "C64" });
+
+const createOverlayCanvas = ({tokenId, level, charLimit }, width, height) => {
+  // Create an overlay canvas and draw your overlay text once.
+  const overlayCanvas = createCanvas(width, height);
+  const overlayCtx = overlayCanvas.getContext("2d");
+
+  overlayCtx.clearRect(0, 0, width, height);
+  overlayCtx.font = "bold 20px C64";
+  overlayCtx.textAlign = "center";
+  overlayCtx.fillStyle = "black";
+
+  // align text on overlay convas
+  tokenId && overlayCtx.fillText(`${endChar} DISK ID #${tokenId.padStart(3, '0')} ${endChar}`, width / 2, 75)
+  level && overlayCtx.fillText(`${endChar} LEVEL ${level.padStart(2, '0')} ${endChar}`, width / 2, height - 95);
+  charLimit && overlayCtx.fillText(`${endChar} CHAR LIMIT ${charLimit.padStart(5, '0') || "N/A"} ${endChar}`, width / 2, height - 60);
+  charLimit && overlayCtx.fillText(`[mint to write]`, width / 2, 100)
+
+  return overlayCanvas
+}
+
+const applyOverlay = (decodedGif, overlayCanvas, width, height) => {
+  return decodedGif.frames.map(frame => {
+    const frameCanvas = createCanvas(width, height);
+    const frameCtx = frameCanvas.getContext("2d");
+
+    const imageData = new ImageData(
+      new Uint8ClampedArray(frame.bitmap.data),
+      frame.bitmap.width,
+      frame.bitmap.height
+    )
+    
+    // draw gif and overlay convas on top
+    frameCtx.putImageData(imageData, 0, 0);
+    frameCtx.drawImage(overlayCanvas, 0, 0, width, height);
+
+    const compositeImageData = frameCtx.getImageData(0, 0, width, height);
+    const bitmapImage = new BitmapImage({
+      width: compositeImageData.width,
+      height: compositeImageData.height,
+      data: Buffer.from(compositeImageData.data)
+    });
+
+    return new GifFrame(bitmapImage)
+  })
+}
 
 export async function GET(req) {
   try {
@@ -19,72 +70,28 @@ export async function GET(req) {
     }
 
     // Load the GIF from disk
-    const gifPath = path.join(process.cwd(), "public", "disk_black_medium.gif");
     if (!fs.existsSync(gifPath)) {
       console.error("GIF file not found at path:", gifPath);
       return NextResponse.json({ error: "GIF file not found" }, { status: 500 });
     }
 
-    const gifBuffer = fs.readFileSync(gifPath);
+    const decodedGif = await decodedGifPromise
 
-    // Decode the GIF using GifCodec
-    const codec = new GifCodec();
-    const decodedGif = await codec.decodeGif(gifBuffer);
+    const width = 500;
+    const height = 500;
 
-    // Use GIF dimensions if available, otherwise default to 500x500
-    const width = decodedGif.width || 800;
-    const height = decodedGif.height || 800;
+    const overlayCanvas = createOverlayCanvas({
+      tokenId,
+      level,
+      charLimit
+    }, width, height)
 
-    const endChar = '*'
-
-    // Create an overlay canvas and draw your overlay text once.
-    const overlayCanvas = createCanvas(width, height);
-    const overlayCtx = overlayCanvas.getContext("2d");
-
-    overlayCtx.clearRect(0, 0, width, height);
-    overlayCtx.font = "bold 24px C64";
-    overlayCtx.textAlign = "center";
-    overlayCtx.fillStyle = "black";
-
-    // align text on overlay convas
-    tokenId && overlayCtx.fillText(`${endChar} DISK ID #${tokenId.padStart(3, '0')} ${endChar}`, width / 2, 75)
-    level && overlayCtx.fillText(`${endChar} LEVEL ${level.padStart(2, '0')} ${endChar}`, width / 2, height - 95);
-    charLimit && overlayCtx.fillText(`${endChar} CHAR LIMIT ${charLimit.padStart(5, '0') || "N/A"} ${endChar}`, width / 2, height - 70);
-    charLimit && overlayCtx.fillText(`[mint to write]`, width / 2, 100)
-
-    // Process each decoded frame and create new GifFrames with the overlay applied.
-    const newFrames = [];
-    const bitmapImages = []
-    for (let i = 0; i < decodedGif.frames.length; i++) {
-      const frame = decodedGif.frames[i];
-      
-      // Create a canvas for this frame.
-      const frameCanvas = createCanvas(width, height);
-      const frameCtx = frameCanvas.getContext("2d");
-
-      let imageData;
-
-      if (frame.bitmap.data) {
-        imageData = new ImageData(new Uint8ClampedArray(frame.bitmap.data), frame.bitmap.width, frame.bitmap.height);
-      }
-
-      // draw gif and overlay convas on top
-      frameCtx.putImageData(imageData, 0, 0);
-      frameCtx.drawImage(overlayCanvas, 0, 0, width, height);
-
-      // Get the composite image data.
-      const compositeImageData = frameCtx.getImageData(0, 0, width, height);
-
-      const bitmapImage = new BitmapImage({
-        width: compositeImageData.width,
-        height: compositeImageData.height,
-        data: Buffer.from(compositeImageData.data)
-      });
-
-      // Create a new GifFrame with the composite image data.
-      const newFrame = new GifFrame(bitmapImage)
-      newFrames.push(newFrame);
-    }
+    const newFrames = applyOverlay(
+      decodedGif,
+      overlayCanvas,
+      width,
+      height
+    )
 
     // Encode the quantized frames into a GIF.
     const encodedGif = await codec.encodeGif(newFrames, { loops: 0 });
